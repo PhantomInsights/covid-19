bdad170d2a8a0fee30e0fa99badc1511dc0194b2
agentphantom <phantom@phantom.im>
2020-08-11 12:50:11 -0500

    Updated workflow.

--- scripts/replacer.py
+++ scripts/replacer.py
@@ -2,3 +2,6 @@ import os
 
 os.replace("./global_data.csv", "../data/global_data.csv")
 print("Replaced global file.")
+
+os.replace("./mx_data.csv", "../data/mx_data.csv")
+print("Replaced Mexico file.")
