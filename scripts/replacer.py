c56572494e406915b550977d89c776a2035796b4
agentphantom <phantom@phantom.im>
2020-08-12 09:56:23 -0500

    Updated datasets.

--- scripts/replacer.py
+++ scripts/replacer.py
@@ -1,7 +1,14 @@
 import os
+import zipfile
+
 
 os.replace("./global_data.csv", "../data/global_data.csv")
 print("Replaced global file.")
 
-os.replace("./mx_data.csv", "../data/mx_data.csv")
+with zipfile.ZipFile("./mx_data.zip", "w", compression=zipfile.ZIP_DEFLATED) as zip_file:
+    print("Compressing Mexico file.")
+    zip_file.write("mx_data.csv")
+
+os.replace("./mx_data.zip", "../data/mx_data.zip")
+os.remove("./mx_Data.csv")
 print("Replaced Mexico file.")
